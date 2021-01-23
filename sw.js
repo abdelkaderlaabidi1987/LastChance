6ce44139ae9d695a62600471ba28ea3616ce9d9e
abdelkaderlaabidi1987 <60947747+abdelkaderlaabidi1987@users.noreply.github.com>
2021-01-23 03:00:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
 self.addEventListener('install', evt=>{
-    console.log(evt);
+    console.log('service worker has been installed');
                 
 });
 
 self.addEventListener('activate', evt => {
-    console.log(evt);
+    console.log('service worker has been activeted');
 });
 
 self.addEventListener('fetch', evt=>{
-    if (!navigator.onLine){
-        evt.respondWith( new Response('pas de connexion yza wld lkalba'));
-    }
-    console.log(evt.request.url);
+    
 });
